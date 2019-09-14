f934e425b5440702f0be066f4da5dc4d09214068
Noble Obioma <nobioma1@gmail.com>
2019-09-14 09:33:47 +0100

    Apply global styles using styled-jsx

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,11 +1,43 @@
 import Layout from '../../components/Layout';
 import fetch from 'isomorphic-unfetch';
+import Markdown from 'react-markdown';
 
 const Post = ({ show }) => (
   <Layout>
     <h1>{show.name}</h1>
-    <p>{show.summary.replace(/<[/]?p>/g, '')}</p>
     <img src={show.image.medium} />
+    <p>{show.summary.replace(/<[/]?p>/g, '')}</p>
+    <div className="markdown">
+      <Markdown
+        source={`
+### This is a title from a markdown
+Yes. We can have a [link](/link).
+And we can have a title as well.
+
+And here's the content.
+      `}
+      />
+    </div>
+    <style jsx global>{`
+      .markdown {
+        font-family: 'Arial';
+      }
+
+      .markdown a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      .markdown a:hover {
+        opacity: 0.6;
+      }
+
+      .markdown h3 {
+        margin: 0;
+        padding: 0;
+        text-transform: uppercase;
+      }
+    `}</style>
   </Layout>
 );
 
